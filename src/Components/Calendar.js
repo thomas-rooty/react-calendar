1f0d592cd06637c86cd36f2377b853f569ef7106
thomas-rooty <essainono1@gmail.com>
2022-03-02 15:15:01 +0100

    fetching events from calendar

--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -1,12 +1,22 @@
 import React from 'react'
-import FullCalendar from '@fullcalendar/react' // must go before plugins
-import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import {getTodos} from '../services/getTodos';
 
 const Calendar = () => {
+    // Fetch events from getTodos
+    const [events, setEvents] = React.useState([]);
+    React.useEffect(() => {
+        getTodos().then(res => {
+            setEvents(res);
+        })
+    }, []);
+
     return (
         <FullCalendar
             plugins={[dayGridPlugin]}
             initialView="dayGridMonth"
+            events={events}
         />
     )
 }
---
+++ src/services/getTodos.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const getTodos = () => {
+    const api = "http://localhost:5000/api/events";
+    return fetch(api)
+        .then(res => res.json())
+        .catch(err => console.log(err));
+};
