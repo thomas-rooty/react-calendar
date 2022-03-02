9ee908d1cb344fa9e4fef7a40786744e5ecb3768
thomas-rooty <essainono1@gmail.com>
2022-03-02 16:14:25 +0100

    affecting _id to events.id

--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -2,21 +2,35 @@ import React from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import {getTodos} from '../services/getTodos';
+import interactionPlugin from "@fullcalendar/interaction";
+
 
 const Calendar = () => {
     // Fetch events from getTodos
     const [events, setEvents] = React.useState([]);
     React.useEffect(() => {
         getTodos().then(res => {
-            setEvents(res);
+            res.forEach(todo => {
+                setEvents(prevEvents => [...prevEvents, {
+                    title: todo.title,
+                    start: todo.start,
+                    end: todo.end,
+                    id: todo._id
+                }])
+            })
         })
     }, []);
 
+    const handleEventClick = (arg) => {
+        console.log(arg.event.id);
+    }
+
     return (
         <FullCalendar
-            plugins={[dayGridPlugin]}
+            plugins={[dayGridPlugin, interactionPlugin]}
             initialView="dayGridMonth"
             events={events}
+            eventClick={handleEventClick}
         />
     )
 }
--- src/services/getTodos.js
+++ src/services/getTodos.js
@@ -6,3 +6,10 @@ export const getTodos = () => {
         .then(res => res.json())
         .catch(err => console.log(err));
 };
+
+export const getTodosByDate = (date) => {
+    const api = `http://localhost:5000/api/events/${date}`;
+    return fetch(api)
+        .then(res => res.json())
+        .catch(err => console.log(err));
+};
